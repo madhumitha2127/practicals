9daeb42bbbd91a14ac620234bf4a75a7f2461943
madhumitha203617 <a.madhumithaarunkumar@gmail.com>
2025-08-24 09:17:41 +0530

    added

--- Main.java
+++ Main.java
@@ -1,21 +1,37 @@
-class student{
-    String name;
-    int roll_no;
-    int age;
+class Student {
+String name;
+int roll_no;
+int age;
 
-    void display(){
-        System.out.println("Name:"+name);
-        System.out.println("Roll No:"+roll_no);
-
-
-    }
+void display() {
+    System.out.println("Name: " + name);
+    System.out.println("Roll No: " + roll_no);
+    System.out.println("Age: " + age);
 }
-public class Main{
-    public static void main(String[] args) {
-        student s = new student();
-        s.name = "Maha";
-        s.roll_no = 73;
-        s.age = 20;
-        s.display();
-    }
+
+
+}
+
+public class Main {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+
+    Student s = new Student();
+
+    System.out.print("Enter Name: ");
+    s.name = sc.nextLine();
+
+    System.out.print("Enter Roll No: ");
+    s.roll_no = sc.nextInt();
+
+    System.out.print("Enter Age: ");
+    s.age = sc.nextInt();
+
+    System.out.println("\n--- Student Details ---");
+    s.display();
+
+    sc.close();
+}
+
+
 }
